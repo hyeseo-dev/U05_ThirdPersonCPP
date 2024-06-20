3e7c00327267e76155117c982f03a465163a4dff
NamgungHyeseo <hyeseo.dev@gmail.com>
2024-06-20 15:13:39 +0900

    0620_init

---
+++ Source/ToreThirdPersonCPP/ToreThirdPersonCPPGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ToreThirdPersonCPPGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TORETHIRDPERSONCPP_API AToreThirdPersonCPPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
