975b64087d02a9559cf84b08bc9c41d2ed7897d1
NamgungHyeseo <hyeseo.dev@gmail.com>
2024-06-27 17:50:12 +0900

    Create Enemy

---
+++ Source/ToreThirdPersonCPP/Characters/CEnemy.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Interface/CCharacterInterface.h"
+#include "CEnemy.generated.h"
+
+class UCAttributeComponent;
+class UCStateComponent;
+class UCMontagesComponent;
+class UCActionComponent;
+
+UCLASS()
+class TORETHIRDPERSONCPP_API ACEnemy : public ACharacter, public ICCharacterInterface
+{
+	GENERATED_BODY()
+
+public:
+	ACEnemy();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	void ChangeBodyColor(FLinearColor InColor) override;
+
+protected:
+	UPROPERTY(BlueprintReadOnly, VisibleDefaultsOnly, Category = "Components")
+	UCAttributeComponent* AttributeComp;
+
+	UPROPERTY(BlueprintReadOnly, VisibleDefaultsOnly, Category = "Components")
+	UCStateComponent* StateComp;
+
+	UPROPERTY(BlueprintReadOnly, VisibleDefaultsOnly, Category = "Components")
+	UCMontagesComponent* MontagesComp;
+
+	UPROPERTY(BlueprintReadOnly, VisibleDefaultsOnly, Category = "Components")
+	UCActionComponent* ActionComp;
+
+private:
+	UMaterialInstanceDynamic* BodyMaterial;
+	UMaterialInstanceDynamic* LogoMaterial;
+};
