2053ca6e2724414c1d1fb102870603f951439406
NamgungHyeseo <hyeseo.dev@gmail.com>
2024-07-16 15:40:53 +0900

    Create: CBTTaskNode_ChangeAction

---
+++ Source/ToreThirdPersonCPP/AI/CBTTaskNode_ChangeAction.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "Components/CActionComponent.h"
+#include "CBTTaskNode_ChangeAction.generated.h"
+
+
+UCLASS()
+class TORETHIRDPERSONCPP_API UCBTTaskNode_ChangeAction : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+public:
+	UCBTTaskNode_ChangeAction();
+
+public:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	virtual void TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	EActionType Type;
+};
